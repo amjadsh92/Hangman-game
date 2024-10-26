32c02124bcaf8e660bf54732f1a85713d15df189
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-26 12:10:58 +0300

    generate a random word whem the file reloads

---
+++ scripts/game.js
@@ -0,0 +1,30 @@
+
+
+words = [
+'information',
+'available',
+'copyright',
+'university',
+'management',
+'international',
+'development',
+'education',
+'community',
+'technology',
+]
+
+
+function randomWholeNum() {
+
+    // Only change code below this line
+  
+   
+    return Math.floor(Math.random()*10);
+  }
+
+index = randomWholeNum()
+
+console.log(words[index])  
+
+
+
