19681d5932502389018c300be2a8bf026c3473ff
amjadsh92 <amjadsharafeddien@gmail.com>
2024-10-26 19:18:28 +0300

    Let a random word appear in the answer section by manipulating the dom

--- scripts/game.js
+++ scripts/game.js
@@ -22,9 +22,41 @@ function randomWholeNum() {
     return Math.floor(Math.random()*10);
   }
 
-index = randomWholeNum()
+index = randomWholeNum();
 
-console.log(words[index])  
+//console.log(words[index])  
+
+var randomword = words[index]
+
+
+
+
+function updatedRandomWord(){
+    n = randomword.length
+    var updatedRandomWord = ""
+    for (let i = 0; i < n; i++){
+
+        updatedRandomWord += randomword[i] + " "
+    }
+
+    return updatedRandomWord
+
+
+}
+
+var updatedword = updatedRandomWord(randomword) 
+
+
+
+const node = document.createElement("p");
+const textnode = document.createTextNode(updatedword);
+node.appendChild(textnode);
+document.getElementById("answer-section").appendChild(node);
+
+
+
+
+
 
 
 
